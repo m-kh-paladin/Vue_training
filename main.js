db4d0c673177b9f51dd4fc6d9ef602495868b969
m-kh-paladin <m.paladin.wiz@gmail.com>
2023-07-04 17:07:46 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        product : "Soocks",
+        description : "this soocks is one the best in the world",
+        link: "https://www.google.com/",
+        inSale : true
+    }
+})
