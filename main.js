b10592115e26e4bcfaaec16321d966bc07403ae7
m-kh-paladin <m.paladin.wiz@gmail.com>
2023-07-24 11:34:46 +0330

    add button for decrease count of product and show sizes of product

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var app = new Vue({
     el: "#app",
     data: {
-        product : "Socks",
-        description : "this soocks is one the best in the world",
+        product: "Socks",
+        description: "this soocks is one the best in the world",
         image: './assets/image_1.jpg',
         link: "https://www.google.com/",
-        inventory : 100,
-        inSale : false   // CHALLENGE PROPERTY
+        inventory: 100,
+        inSale: false,   // CHALLENGE PROPERTY
+        sizes: ["small", "medium", "big"],
+        card: 5,
+    },
+    methods: {
+        decCard() {
+            this.card -= 1;
+        }
     }
 })
